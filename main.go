c0a9e4f744eacd22ebd284b9debb80d468e7da39
Gabriele Diener <gabriele.diener@caffeina.com>
2019-01-17 12:11:38 +0100

    Added log on server

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[INFO]: Access on %q", r.RequestURI)
 		fmt.Fprintf(w, "Hello world!")
 	}))
+	log.Println("[INFO]: Hello world app is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
